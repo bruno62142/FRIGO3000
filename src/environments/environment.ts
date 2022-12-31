8b5286d9e73cec74a3f74b9f0edd6fff40265599
bruno62142 <brunojuniorlenglet@gmail.com>
2022-12-31 16:39:17 +0100

    Redi Git

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment ={
+  firebase: {
+    projectId: 'frigo-3000-bc448',
+    appId: '1:181115185198:web:0a65eeebcef8e6c36657bf',
+    databaseURL: 'https://frigo-3000-bc448-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'frigo-3000-bc448.appspot.com',
+    apiKey: 'AIzaSyC03Gqms_Tm93Fal6oncfeVMVUQVDZ1Koo',
+    authDomain: 'frigo-3000-bc448.firebaseapp.com',
+    messagingSenderId: '181115185198',
+    measurementId: 'G-HRMHFH6RHC',
+  },
+    production: false
+};
