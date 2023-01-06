efe029b5c52a40688550249b556892388f30f1f4
bruno62142 <brunojuniorlenglet@gmail.com>
2023-01-06 22:10:50 +0100

    last version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireObject,AngularFireList } from '@angular/fire/compat/database';
+import {map} from 'rxjs';
+import {ESP32_Data} from './model' 
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
 })
-export class AppComponent {
-  title = 'FRIGO3000';
-}
+
+export class AppComponent implements OnInit{
+  private historyRef: AngularFireList<ESP32_Data>;
+  private currentRef: AngularFireObject<ESP32_Data>;
+  public history?: ESP32_Data[];
+  public current?: ESP32_Data|null;
+
+  constructor(db: AngularFireDatabase){
+    this.historyRef= db.list<ESP32_Data>('/history');
+    this.currentRef= db.object<ESP32_Data>('/current');
+
+  }
+
+
+  ngOnInit(): void{
+    this.historyRef
+    .snapshotChanges()
+    .pipe(map((changes) => changes.map((c) => ({ ...c.payload.val()}))))
+    .subscribe((data) => {
+      this.history=data; 
+      this.current=data[data.length-1]
+    });
+    this.currentRef.snapshotChanges().subscribe((data) => {
+      //this.current=data.payload.val();
+    })
+  }
+
+  }
--- src/app/model.ts
+++ src/app/model.ts
@@ -1,34 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase, AngularFireObject,AngularFireList } from '@angular/fire/compat/database';
-import {map} from 'rxjs';
-import {ESP32_Data} from '../models' 
-@Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
-})
+export interface ESP32_Data{
 
-export class AppComponent implements OnInit{
-  private historyRef: AngularFireList<ESP32_Data>;
-  private currentRef: AngularFireObject<ESP32_Data>;
-  constructor(db: AngularFireDatabase){
-    this.historyRef= db.list<ESP32_Data>('/history');
-    this.currentRef= db.object<ESP32_Data>('/current');
+  date?: string;
+  humidity?: number;
+  temperature?: number;
   }
-
-
-  ngOnInit(): void{
-    this.historyRef
-    .snapshotChanges()
-    .pipe(map((changes) => changes.map((c) => ({ ...c.payload.val()}))))
-    .subscribe((data) => {
-      this.history=data; 
-      this.current=data[data.length-1]
-    });
-    this.currentRef.snapshotChanges().subscribe((data) => {
-      //this.current=data.payload.val();
-    })
-  }
-
-  }
-
