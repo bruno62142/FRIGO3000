3481df10061ec04f1156c53a1835ec3c05f6891b
bruno62142 <brunojuniorlenglet@gmail.com>
2023-01-27 23:56:22 +0100

    almost
    ya plus que les graph, le tableau de défaut et un peu de style.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import {map} from 'rxjs';
 import {ESP32_Data} from './model' 
 import flatpickr from 'flatpickr';
 import Chart from 'chart.js/auto';
+import { Renderer2 } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -14,19 +16,23 @@ import Chart from 'chart.js/auto';
 export class AppComponent implements OnInit{
   title= 'FRIGO3000';
   private historyRef: AngularFireList<ESP32_Data>;
+  private test: AngularFireList<ESP32_Data>;
   private currentRef: AngularFireObject<ESP32_Data>;
   private history_DefRef: AngularFireList<ESP32_Data>;
 
   public history?: ESP32_Data[];
   public history_def?: ESP32_Data[];
   public current?: ESP32_Data|null;
-  public chart_t: any;
-  public chart_h:any;
+  public chart_t?: any;
+  public chart_h?:any;
+  public selectedOption?: string;
+  public temperature?:ESP32_Data[];
 
-  constructor(db: AngularFireDatabase){
+  constructor(db: AngularFireDatabase,private renderer: Renderer2){
     this.historyRef= db.list<ESP32_Data>('/history');
     this.history_DefRef= db.list<ESP32_Data>('/history_def');
     this.currentRef= db.object<ESP32_Data>('/current');
+    this.test=db.list<ESP32_Data>('/history/temperature');
 
   }
 
@@ -45,12 +51,34 @@ export class AppComponent implements OnInit{
     .pipe(map((changes) => changes.map((c) => ({ ...c.payload.val()}))))
     .subscribe((data) => {
       this.history=data; 
-      this.current=data[data.length-1];
-     // this.createLineChart()
+      this.createLineChart()
+    });
+    this.test
+    .snapshotChanges()
+    .pipe(map((changes) => changes.map((c) => ({ ...c.payload.val()}))))
+    .subscribe((data) => {
+      this.temperature=data; 
+      
     });
     this.currentRef.snapshotChanges().subscribe((data) => {
       this.current=data.payload.val();
-    })
+      if(this.current && this.current.TempThermostat) {
+        let thermoInput = <HTMLInputElement>document.getElementById("thermo");
+        if(thermoInput){
+          thermoInput.value = this.current.TempThermostat.toString();
+        }
+      }
+
+      if(this.current && this.current.Cmusique) {
+        let MusiqueInput = <HTMLInputElement>document.getElementById("muse");
+        if(MusiqueInput){
+          MusiqueInput.value = this.current.Cmusique.toString();
+        }
+      }
+      
+    
+     
+    });
     flatpickr("#datetimepicker-dashboard", {
       inline: true,
       prevArrow: "<span title='Mois précédent'>&laquo;</span>",
@@ -58,56 +86,64 @@ export class AppComponent implements OnInit{
       defaultDate: new Date(Date.now() - 5 * 24 * 60 * 60 * 1000)
     });
   }
-  createLineChart() {
-    if(this.history) {
+  updateThermostat() {
+    let thermoInput = <HTMLInputElement>document.getElementById("thermo");
+    if(thermoInput){
+      let thermoValue = parseFloat(thermoInput.value);
+      this.currentRef.update({TempThermostat: thermoValue});
 
-       // Récupérer les données de température de l'historique
-    let temperatureData = this.history.map(data => data.temperature);
-    // Récupérer les données d'humidité de l'historique
-    //let humidityData = this.history.map(data => data.humidity);
-
-    // Récupérer les heures de l'historique
-    let labels = this.history.map(data => data.time);
-        this.chart_t = new Chart("chartjs-dashboard-line", {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [
-            {
-                label: "temperature",
-                data: temperatureData,
-                backgroundColor: 'red'
-            }
-            ]
-        },
-        options: {
-            aspectRatio:2.5
-        }
-        });
-
-        /*this.chart_h = new Chart("chartjs-dashboard-bar", {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [
-            {
-                label: "Humidité",
-                data: humidityData,
-                backgroundColor: 'blue'
-            }
-            ]
-        },
-        options: {
-            aspectRatio:2.5
-        }
-        });*/
     }
 }
+updateMusique()
+{
+  let MusiqueInput = <HTMLInputElement>document.getElementById("muse");
+  if(MusiqueInput){
+    let museValue = parseFloat(MusiqueInput.value);
+    this.currentRef.update({Cmusique: museValue});
+
+  }
+}
+createLineChart() {
+  if(this.history) {
+
+    if(!this.chart_t) {
+
+      // Récupérer les données de température de l'historique
+      
+  let temperatureData = this.history.map(data => data.temperature);
+  
+
+  // Récupérer les données d'humidité de l'historique
+  let humidityData = this.history.map(data => data.humidity);
+
+  // Récupérer les heures de l'historique
+  let labels = this.history.map(data => data.time);
+
+      this.chart_t = new Chart(this.renderer.selectRootElement('#chartjs-dashboard-line'), {
+      type: 'line',
+      data: {
+          labels: labels,
+          datasets: [
+          {
+              label: "temperature",
+              data:[0],
+              backgroundColor: 'red'
+          },
+          
+          ]
+      },
+      options: {
+        
+      }
+  });
+}
+  }
 
 
   
 
 } 
+}
 
   
 
